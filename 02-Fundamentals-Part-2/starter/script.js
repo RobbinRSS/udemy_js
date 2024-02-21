896032659a83511e9df3fa7db6c6573b5a5c62b1
EAVizion <robbinschrijver07@gmail.com>
2024-02-21 15:30:28 +0100

    array_operators

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -122,7 +122,7 @@ function checkWinner(avgDolphins, avgKoalas) {
 }
 
 checkWinner(scoreDolphins, scoreKoalas);
-*/
+
 
 const friend1 = 'Michael';
 const friend2 = 'Steven';
@@ -159,3 +159,36 @@ console.log(age1, age2, age3)
 
 const ages = [calcAge2(years[0]), calcAge2(years[1]), calcAge2(years[2])];
 console.log(ages);
+
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// remove elements
+friends.pop(); //removes last
+const popped = friends.pop(); 
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // removes First
+console.log(friends)
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven')
+}
+*/
