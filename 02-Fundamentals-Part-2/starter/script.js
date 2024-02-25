e70501b2bd1d473de2c7dd3c8c6fde28822f3f8a
EAVizion <robbinschrijver07@gmail.com>
2024-02-25 20:12:21 +0100

    loop_inside_loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -317,7 +317,7 @@ if (mark.bmi > john.bmi) {
 for(let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep} `)
 };
-*/
+
 
 const jonas = [
     'Jonas',
@@ -363,3 +363,25 @@ for(let i = 0; i < jonas.length ; i++) {
     if(typeof jonas[i] === 'number') break;
     console.log(jonas[i], typeof jonas[i])
 };
+*/
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for(let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--- starting exercise ${exercise}`);
+
+    for(let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    }
+}
