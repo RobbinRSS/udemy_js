be892252256148057351246a3ebc9c9514961685
EAVizion <robbinschrijver07@gmail.com>
2024-02-25 21:08:32 +0100

    function_with_loop_avg

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -420,7 +420,19 @@ const totals = [];
 for(let i = 0; i < bills.length; i++) {
     const tip = calcTip(bills[i]);
     tips.push(tip);
-    totals.push(bills[i] + tips[i])
+    totals.push(bills[i] + tip);
 }
-console.log(tips);
-console.log(totals);
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        // sum = sum + arr[i];
+        sum += arr[i];
+    }
+    return sum / arr.length
+}
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
