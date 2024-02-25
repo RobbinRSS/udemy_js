04b3392fc5d7f60c995ed4a82d53ee914b890c89
EAVizion <robbinschrijver07@gmail.com>
2024-02-25 20:32:54 +0100

    while_loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -363,7 +363,7 @@ for(let i = 0; i < jonas.length ; i++) {
     if(typeof jonas[i] === 'number') break;
     console.log(jonas[i], typeof jonas[i])
 };
-*/
+
 
 const jonas = [
     'Jonas',
@@ -384,4 +384,24 @@ for (let exercise = 1; exercise < 4; exercise++) {
     for(let rep = 1; rep < 6; rep++) {
         console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
     }
+}
+*/
+
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(` Lifting weights repetition ${rep}`);
+// }
+
+let rep = 1;
+while(rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+//use a while if you dont know how many times it will loop
+while (dice !== 6) {
+    console.log(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
 }
