92d72b3ea328fb7ff941f0b3345e8f4862491ced
RobbinRSS <robbinschrijver07@gmail.com>
2024-03-01 14:12:00 +0100

    forecast_function

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -89,4 +89,21 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY
 console.log(measureKelvin());
+
+
+// Problem = printing the different days in different strings
+// Subproblem = loop the function
+
+const data1 = [17, 21, 34];
+const data2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}C in ${i + 1} days ... `;
+  }
+  console.log("..." + str);
+}
+
+printForecast(data1);
 */
