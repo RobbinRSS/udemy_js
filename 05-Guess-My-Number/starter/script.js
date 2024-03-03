8c3aa91149aa4a50f8cd94ff733b3583623914ec
RobbinRSS <robbinschrijver07@gmail.com>
2024-03-03 16:59:07 +0100

    CSS_practice_and_DOM_explaining

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+// query = selecting a class from the html
+// textcontent = shows the text that is connected with .message
+console.log(document.querySelector('.message').textContent);
