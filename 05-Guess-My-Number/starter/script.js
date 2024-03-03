96cdfe2b4682331cfdf51bb325600fe1c8f0b9fe
RobbinRSS <robbinschrijver07@gmail.com>
2024-03-03 17:19:57 +0100

    basics_queryselector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,16 @@
 'use strict';
+// explenation child elements
+// html = first child
+//<head> or <body> are the second child
+// inside the head or body are also children
 
-// query = selecting a class from the html
+// querySelector = selecting a class from the html
 // textcontent = shows the text that is connected with .message
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
