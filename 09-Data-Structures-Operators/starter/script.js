5dca94e1afd74c9ca2a0cd0d8b1f6d1e367b7d5a
RobbinRSS <robbinschrijver07@gmail.com>
2024-03-21 11:31:49 +0100

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,41 @@ const restaurant = {
   },
 };
 
+///////// logical assignement operators //////////////
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// ----------- Does the same thing ------------ //
+// OR assignement operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+// -------------------------------------------- //
+
+/*
 /////////// The Nullish Coalescing operator(??) ///////////
 restaurant.numGuests = 0;
 const guests2 = restaurant.numGuests || 10;
@@ -60,9 +95,8 @@ console.log(guests2);
 // Nullish: null and undefined (NOT 0 or '') if restaurant.numGuests = undefined or null the evalution continues to 10
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
-//////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
-/*
 /////////////// Short circuiting (&& and ||) /////////////////
 
 // Use ANY data type, return ANY data type, short-circuiting
