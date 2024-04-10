8e1d23ca85c17a300ce05dede8fa20c1f53b4c55
RobbinRSS <robbinschrijver07@gmail.com>
2024-04-10 10:14:52 +0200

    coding challenge 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -51,6 +51,75 @@ const restaurant = {
   },
 };
 
+/////////////////// Coding challenge #2 //////////////////
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1)
+for (const [index, player] of game.scored.entries()) {
+  console.log(`${player} scored goal number ${index + 1}`);
+}
+
+// 2)
+let sum = 0;
+let count = 0;
+
+for (const odd in Object.values(game.odds)) {
+  sum += odd;
+  count++;
+}
+
+const average = sum / count;
+console.log(average);
+
+// 3)
+for (const [key, value] of Object.entries(game.odds)) {
+  if (key === 'x') {
+    console.log(`chance of draw: ${value}`);
+  } else {
+    console.log(`Odd of victory ${game[key]}: ${value}`);
+  }
+}
+
 /*
 
 ////// looping objects: objects keys / values / entries //////
