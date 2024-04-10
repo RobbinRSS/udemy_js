b95224bfdbc3639c683896932488e9e223e5a6cb
RobbinRSS <robbinschrijver07@gmail.com>
2024-04-10 13:57:57 +0200

    update

--- 09-Data-Structures-Operators/assignments/assignement.js
+++ 09-Data-Structures-Operators/assignments/assignement.js
@@ -444,3 +444,23 @@ const uniqueKeywordsArr = [...uniqueKeywords];
 
 // 12.6
 uniqueKeywords.clear();
+
+// Maps: fundementals
+
+// 13.1
+const bookMap = new Map([
+  ["title", "Clean Code"],
+  ["author", "Robert C. Martin"],
+]);
+
+// 13.2
+bookMap.set("pages", 464);
+
+// 13.3
+console.log(`${bookMap.get("title")} by ${bookMap.get("author")}`);
+
+// 13.4
+console.log(bookMap.size);
+
+// 13.5
+if (bookMap.has("author")) console.log("The author of the book is known");
