b0891c9a9a6f16683cdac4611897884346e2d9f5
RobbinRSS <robbinschrijver07@gmail.com>
2024-05-08 15:53:44 +0200

    update

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -26,7 +26,6 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
 
 const flight = 'LH234';
 const jonas = {
@@ -55,3 +54,38 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+*/
+
+//////////  functions accepting callback functions //////////
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// higher-order function
+const transformer = function (str, fn) {
+  console.log(`original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+////////////////////////////////////////////////////////////
