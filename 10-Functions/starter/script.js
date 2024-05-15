420ad101832052debac6a4a02c9ca95d7ef48adb
RobbinRSS <robbinschrijver07@gmail.com>
2024-05-15 22:03:43 +0200

    more closure examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -274,8 +274,6 @@ addTax3(0.23)(100);
 const addVAT3 = addTax3(0.23);
 console.log(addVAT3(100));
 
-*/
-
 ////////////////// coding challenge 1 ////////////////////
 const poll = {
   question: 'What is your favorite programming language?',
@@ -354,3 +352,46 @@ console.dir(booker);
 
 // EVEN EASIER EXPLENATION = a closure is like a backpack that a function carries around wherever it goes. this backpack has all the variables that were present in the enviroment where the function was created
 // function -> closure(like a backpack) -> variables(inside the closure (backpack) )
+
+*/
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// re-assiging f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// const perGroup = 1000; closure gets priority so this perGroup will not be executed but the closure perGroup inside the function will get executed
+boardPassengers(180, 3);
