ca3e90546282a32f2c742db5757df69c9160a0aa
RobbinRSS <robbinschrijver07@gmail.com>
2024-05-22 10:01:20 +0200

    arr methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,35 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2)); // last 2 elements of an array
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2)); // [b, c]
+console.log(arr.slice()); // all the elements from the array
+console.log([...arr]); // all the elements from the array
+
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr); // splice deleted the oter ones from the array so it does mutate the original arrayh
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2); // reverse mutates the original array
+
+// CONCAT
+const letters = arr.concat(arr2); // arr + arr2 and makes it a single array
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - '));
