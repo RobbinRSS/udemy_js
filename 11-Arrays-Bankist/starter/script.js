90cb5c908a9f1462b6ba354575190abba9d59dcf
RobbinRSS <robbinschrijver07@gmail.com>
2024-05-23 13:28:43 +0200

    filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -46,8 +46,6 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-console.log(accounts);
-console.log(account1);
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -235,3 +233,15 @@ const movementsDescriptions = movements.map((mov, i) => {
 });
 console.log(movementsDescriptions);
 */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(mov => mov > 0);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawal = movements.filter(mov => mov < 0);
+console.log(withdrawal);
