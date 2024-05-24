b76c31e376ab208dd5a393e129cca99f9f5284b8
RobbinRSS <robbinschrijver07@gmail.com>
2024-05-24 13:52:08 +0200

    coding challenge 3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -102,7 +102,7 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
+      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -335,7 +335,6 @@ console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 // Jonas made a new variable for the filter method everything else was pretty much the same
 //////////////////////////////////////////////////////////////
-*/
 
 //////////////// The magic of chaining methods ///////////////
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -354,3 +353,24 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositsUSD);
+*/
+// const calcAverageHumanAge = function (ages) {
+//   const humanAge = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18);
+
+//   const averageHumanAge =
+//     humanAge.reduce((acc, age) => acc + age, 0) / humanAge.length;
+
+//   return Math.round(averageHumanAge);
+// };
+
+///////////////////// coding challenge 3 /////////////////////
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
