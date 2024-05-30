66dc296b28a7b47b3dd5485040653f2d8624755e
RobbinRSS <robbinschrijver07@gmail.com>
2024-05-30 11:54:34 +0200

    start coding challenge

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -600,7 +600,6 @@ labelBalance.addEventListener('click', function () {
 
   console.log(movementsUI.map(el => el.textContent.replace('€', '')));
 });
-*/
 
 ///////////////// Array methods practice ////////////////////
 
@@ -664,3 +663,46 @@ const convertTitleCase = function (title) {
 
 console.log(convertTitleCase('this is a nice title'));
 console.log(convertTitleCase('this is a LONG title but not too long'));
+*/
+
+//////////////////// coding challenge 4 //////////////////////
+// TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+dogs.forEach(dog => (dog.recommendedFood = dog.weight ** 0.75 * 28));
+
+console.log(dogs);
+
+// 2.
+const sarahDog = dogs.find(dog => {
+  const dogSarah = dog.owners.includes('Sarah');
+  if (dogSarah && dog.curFood > dog.recommendedFood * 1.1) {
+    console.log('Your dog is eating too much');
+  } else if (dogSarah && dog.curFood < dog.recommendedFood * 0.9) {
+    console.log('Your dog is eating too little');
+  }
+});
+
+// 3.
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooLittle);
+// 4.
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5.
