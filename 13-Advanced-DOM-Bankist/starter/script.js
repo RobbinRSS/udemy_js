0114482497e134b07f8060a55aa5c71bbd951714
RobbinRSS <robbinschrijver07@gmail.com>
2024-06-14 09:58:45 +0200

    selecting, creating, deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////////////
+
+console.log(document.documentElement); // entire html
+console.log(document.head); // html head
+console.log(document.body); // html body
+
+// node list
+const header = document.querySelector('.header');
+const allSection = document.querySelectorAll('.section');
+console.log(allSection);
+
+//html collection
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); // all the buttons
+
+// html collection
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookie for improved functionality and analytics';
+message.innerHTML =
+  'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); // add element as first child in header
+header.append(message); // add element as last child in header
+
+// header.append(message.cloneNode(true)); // cloning elemenet
+
+// header.before(message); // before the header
+// header.after(message); // after the header
+
+// delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove(); // new way
+
+    // message.parentElement.removeChild(message) // old way
+  });
