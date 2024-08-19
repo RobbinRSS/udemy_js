065da731f236e400b02098af588f91e1d823372d
RobbinRSS <robbinschrijver07@gmail.com>
2024-08-19 10:18:47 +0200

    setters and getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -96,14 +96,15 @@ bmw.accelerate();
 mercedes.brake();
 mercedes.brake();
 */
+//////////// ES6 classes ///////////
 
 // class expression
 // const PersonCl = class {};
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -113,13 +114,29 @@ class PersonCl {
   }
 
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -131,3 +148,27 @@ jessica.greet();
 // 1. classes are NOT hoisted (meaning: you have to declare the function, because every code above it wouldnt work with it)
 // 2. Classes are first-class citizens (meaning: pass them into functions and also return them from functions)
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+////////////////////////
+
+////////// Setters and getters //////////
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.pop();
+  },
+
+  set latest(mov) {
+    // every set needs atleast 1 parameter
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest); // because of get, you dont have to call it like this: account.latest()
+
+account.latest = 50; // becaus of set, you dont have to call it like this: account.latest(50)
+console.log(account.movements);
