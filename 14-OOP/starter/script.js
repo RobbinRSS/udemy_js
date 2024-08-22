09380f79338563fae125d7afd88621a7d3fbab4c
RobbinRSS <robbinschrijver07@gmail.com>
2024-08-22 11:01:55 +0200

    private class fields and methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -432,41 +432,57 @@ console.log(jay);
 */
 
 /////////// Another class example /////////// and
-//// encapsulation: protected properties and methods ////
+//// encapsulation: protected properties and methods //// and
+//// encapsulation: private class fields and methods ////
+
+// public fields
+// private fields
+// public methods
+// private methods
 
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  //  2) private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
     //protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account ${owner}`);
   }
 
+  // 3) public methods
+
   // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
   withdraw(val) {
     this.deposit(-val);
   }
 
-  _approveLoan(val) {
-    return true;
-  }
-
   requestLoan(val) {
-    if (this._approveLoan(val)) {
+    if (this.#approveLoan(val)) {
       this.deposit(val);
       console.log('Loan approved');
     }
+  }
+
+  // 4) private methods
+  #approveLoan(val) {
+    return true;
   }
 }
 
@@ -481,6 +497,7 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 
-console.log(acc1.pin);
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
 
 //////////////////////
