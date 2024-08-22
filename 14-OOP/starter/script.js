9568820f0c17b08a6d9b5741c787e7864b027ca6
RobbinRSS <robbinschrijver07@gmail.com>
2024-08-22 10:27:19 +0200

    another class example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -330,8 +330,6 @@ console.log(tesla);
 tesla.brake();
 tesla.accelerate();
 
-*/
-
 //// inheritance between classes: ES6 classes ////
 class PersonCl {
   constructor(fullName, birthYear) {
@@ -430,3 +428,51 @@ jay.init('Jay', 2010, 'Computer science');
 jay.introduce();
 jay.calcAge();
 console.log(jay);
+///////////////////////
+*/
+
+/////////// Another class example ///////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+
+console.log(acc1.pin);
