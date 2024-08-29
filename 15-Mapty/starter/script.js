899992b11e7bbfd6f90c68864eb758dabb2c9016
RobbinRSS <robbinschrijver07@gmail.com>
2024-08-29 11:31:28 +0200

    Managing Workout data: creating classes

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,8 +11,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10); // converting date to string and then taking last 10 characters
 
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 5.2, 24, 178);
+// console.log(run1, cycling1);
+
+////////////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
@@ -55,6 +98,7 @@ class App {
     this.#map.on('click', this._showForm.bind(this));
   }
 
+  // map event is when something happens on the map like a click
   _showForm(mapE) {
     this.#mapEvent = mapE;
     form.classList.remove('hidden');
