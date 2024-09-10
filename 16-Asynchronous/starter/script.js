5847e22e646cdc16af225d5bda2c62f9b59ee4ff
RobbinRSS <robbinschrijver07@gmail.com>
2024-09-10 14:02:59 +0200

    learning about callback hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,7 +7,30 @@ const countriesContainer = document.querySelector('.countries');
 
 // https://countries-api-836d.onrender.com/countries/
 
-const getCountryData = function (country) {
+const renderCountry = function (data, className = '') {
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(data.currencies)[0].name;
+
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`); // 'GET' get data
 
@@ -15,34 +38,30 @@ const getCountryData = function (country) {
 
   // as soon as the data is arrived, this callback function will be called
   request.addEventListener('load', function () {
-    console.log(this.responseText);
-
+    // console.log(this.responseText);
     const [data] = JSON.parse(this.responseText);
-
     console.log(data);
 
-    const language = Object.values(data.languages)[0];
-    const currency = Object.values(data.currencies)[0].name;
+    // render Country (1)
+    renderCountry(data);
 
-    const html = `
-  <article class="country">
-    <img class="country__img" src="${data.flags.png}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${language}</p>
-      <p class="country__row"><span>💰</span>${currency}</p>
-    </div>
-  </article>
-  `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`); // 'GET' get data
+
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-
-getCountryData('netherlands');
-getCountryData('usa');
-getCountryData('germany');
+getCountryAndNeighbour('netherlands');
